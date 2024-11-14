ff3af493928d0cd55b8db1a5caa47a151532f894
Amirreza <rezaamir731@gmail.com>
2024-11-14 08:48:25 +0330

    docs: -add basic gh pages config

--- documents/docusaurus.config.ts
+++ documents/docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
     url: "https://amirrezaDev1378.github.io",
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: "/",
+    baseUrl: "/ollama-model-direct-download/",
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
