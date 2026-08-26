5477b62c5c4a6f4a8a3b53cb193eafcbb1a7bf1a
Amirreza <rezaamir731@gmail.com>
2026-08-26 11:58:47 +0330

    feat: add GUI build script

--- scripts/build-for-all.sh
+++ scripts/build-for-all.sh
@@ -7,6 +7,7 @@ OUTPUT_DIR="../bin"
 mkdir -p $OUTPUT_DIR
 APP_PREFIX="omdd"
 PLATFORMS=("linux/amd64" "linux/386" "linux/arm64" "linux/arm" "darwin/amd64" "darwin/arm64" "windows/amd64" "windows/386" "freebsd/amd64" "freebsd/386" "freebsd/arm" "openbsd/amd64" "openbsd/386")
+echo "Building the CLI.."
 for PLATFORM in "${PLATFORMS[@]}"
 do
     OS=$(echo "$PLATFORM" | cut -d'/' -f1)
@@ -25,4 +26,33 @@ do
         fi
     done
 
-echo "Build completed!"
+echo "CLI Build completed!"
+
+
+echo "Building the GUI.."
+
+cd ../gui/frontend
+bun i
+cd ../
+
+for PLATFORM in "${PLATFORMS[@]}"
+do
+    OS=$(echo "$PLATFORM" | cut -d'/' -f1)
+    ARCH=$(echo "$PLATFORM" | cut -d'/' -f2)
+    OUTPUT_NAME=./$APP_PREFIX-GUI-$OS-$ARCH
+
+    if [ "$OS" = "windows" ]; then
+        OUTPUT_NAME+='.exe'
+    fi
+
+    echo "Building for $OS/$ARCH..."
+    if ! GOOS=$OS GOARCH=$ARCH wails build -clean -o "$OUTPUT_NAME"
+         then
+            echo "An error occurred during the build for $PLATFORM"
+            exit 1
+        fi
+    done
+
+mv ./build/bin/** ../bin -f
+
+echo "GUI Build completed!"
